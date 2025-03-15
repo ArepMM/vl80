627cf60c93f4fab36eb789314014500aaf6faafe
maisvendoo <maisvendoo@gmail.com>
2025-03-15 15:13:12 +0300

    Заготовка проекта

---
+++ vl80s/include/vl80s.h
@@ -0,0 +1,21 @@
+#ifndef		VL80S_H
+#define		VL80S_H
+
+#include    <vehicle-api.h>
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+class VL80s : public Vehicle
+{
+public:
+
+    VL80s(QObject *parent = Q_NULLPTR);
+
+    ~VL80s();
+
+private:
+
+};
+
+#endif // VL80S_H
