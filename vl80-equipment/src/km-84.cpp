a775d89e2f1dcdc4673439df9e0cac400b450dc1
maisvendoo <maisvendoo@gmail.com>
2025-03-15 16:25:36 +0300

    Реализация главного вала развертки КМ

--- vl80-equipment/src/km-84.cpp
+++ vl80-equipment/src/km-84.cpp
@@ -49,3 +49,25 @@ void ControllerKM84::stepKeysControl(double t, double dt)
 {
 
 }
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ControllerKM84::init_main_shaft()
+{
+    std::fill(main_shaft_state[K_51_52].begin(), main_shaft_state[K_51_52].end(), false);
+    main_shaft_state[K_51_52][POS_FP] = true;
+    main_shaft_state[K_51_52][POS_RP] = true;
+    main_shaft_state[K_51_52][POS_AP] = true;
+
+    std::fill(main_shaft_state[K_53_54].begin(), main_shaft_state[K_53_54].end(), false);
+    main_shaft_state[K_53_54][POS_FP] = true;
+    main_shaft_state[K_53_54][POS_RP] = true;
+    main_shaft_state[K_53_54][POS_AP] = true;
+
+    std::fill(main_shaft_state[K_55_56].begin(), main_shaft_state[K_55_56].end(), false);
+    main_shaft_state[K_55_56][POS_AV] = true;
+    main_shaft_state[K_55_56][POS_RV] = true;
+    main_shaft_state[K_55_56][POS_RP] = true;
+    main_shaft_state[K_55_56][POS_AP] = true;
+}
