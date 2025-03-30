968939e31f23275f9d72cd770d5b5ea27c019483
Mikhail Arepev <arepmm@gmail.com>
2025-03-30 17:05:23 +0300

    Заготовка под пантограф с пневмоприводом. Потом перенести в движок!

---
+++ vl80-equipment/include/pantograph_new.h
@@ -0,0 +1,125 @@
+#ifndef PANTOGRAPH_NEW_H
+#define PANTOGRAPH_NEW_H
+
+#include    "device.h"
+#include    "trigger.h"
+
+//------------------------------------------------------------------------------
+// Токоприемник пантографного типа
+//------------------------------------------------------------------------------
+class /*DEVICE_EXPORT*/ Pantograph_new : public Device
+{
+
+public:
+
+    /// Конструктор
+    Pantograph_new(QObject *parent = Q_NULLPTR);
+
+    /// Деструктор
+    ~Pantograph_new();
+
+    // TODO // Заготовка под взаимодействие с путевой инфраструктурой
+    enum
+    {
+        SIZE_OF_OUTPUTS = 1,    ///< Размер массива исходящих сигналов
+
+        OUTPUT_CURRENT = 0,     ///< Ток, потребляемый из контактной сети, А
+
+        SIZE_OF_INPUTS = 3,     ///< Размер массива входящих сигналов
+
+        INPUT_WIRE_HEIGHT = 0,  ///< Высота контактного провода, м
+        INPUT_VOLTAGE = 1,      ///< Напряжение в контактной сети, В
+        INPUT_FREQUENCY = 2,    ///< Частота тока в контактной сети, Гц
+    };
+
+    /// Задать ток, потребляемый из контактной сети, А
+    void setCurrent(double I);
+
+    /// Задать поток воздуха в цилиндр пневмопривода токоприёмника
+    void setPneumodriveFlow(double flow);
+
+    /// Задать высоту контактного провода, м
+    void setWireHeight(double h);
+
+    /// Задать напряжение в контактной сети, В
+    void setVoltage(double U);
+
+    /// Задать частоту тока в контактной сети, Гц
+    void setFrequency(double f);
+
+    /// Давление в цилиндре пневмопривода токоприёмника, МПа
+    double getPneumodrivePressure() const;
+
+    /// Напряжение на выходе токоприёмника, В
+    double getVoltage() const;
+
+    /// Частота тока на выходе токоприёмника, Гц
+    double getFrequency() const;
+
+    /// Высота полоза токоприёмника, м
+    double getHeight() const;
+
+    /// Относительная высота токоприёмника, д.е.
+    double getRelativeHeight() const;
+
+    // TODO // Переделать озвучку - отделить звуки касания провода и складывания
+    enum {
+        NUM_SOUNDS = 3,
+        CHANGE_SOUND = 0,
+        UP_SOUND = 1,
+        DOWN_SOUND = 2
+    };
+    /// Состояние звука
+    sound_state_t getSoundState(size_t idx = CHANGE_SOUND) const;
+
+    /// Сигнал состояния звука
+    float getSoundSignal(size_t idx = CHANGE_SOUND) const;
+
+private:
+
+    enum {
+        PRESSURE = 0,   ///< Y[0] - Давление пневмопривода токоприёмника, МПа
+        HEIGHT = 1      ///< Y[1] - Высота полоза токоприёмника, м
+    };
+
+    /// Объём цилиндра пневмопривода токоприёмника, м^3
+    double Vc = 0.01;
+
+    /// Поток воздуха в цилиндр пневмопривода токоприёмника
+    double Qc = 0.0;
+
+    /// Минимальное давление срабатывания пневмопривода токоприёмника, МПа
+    double Pmin = 0.35;
+
+    /// Номинальное давление пневмопривода токоприёмника, МПа
+    double Pnom = 0.5;
+
+    /// Время поднятия токоприёмника при номинальном / опускания при нулевом давлении, с
+    double motion_time = 7.0;
+
+    /// Высота полоза в опущенном состоянии, м
+    double height_down = 5.1;
+
+    /// Максимальная высота полоза в поднятом состоянии, м
+    double height_up = 7.0;
+
+    /// Номинальная скорость поднятия / опускания токоприёмника, м/с
+    double motion_v_nom = (7.0 - 5.1) / 7.0;
+
+    /// Признак касания полоза и контактного провода
+    bool is_contact = false;
+
+    /// Допустимая разница высоты полоза и провода, при которой считается касание, м
+    const double eps = 0.05;
+
+    // TODO // Переделать озвучку - отделить звуки касания провода и складывания
+    /// Озвучка состояния токоприемника (true - поднимается, false - опускается)
+    Trigger ref_state;
+
+    void ode_system(const state_vector_t &Y, state_vector_t &dYdt, double t);
+
+    void load_config(CfgReader &cfg);
+
+};
+
+#endif // PANTOGRAPH_NEW_H
---
+++ vl80-equipment/src/pantograph_new.cpp
@@ -0,0 +1,218 @@
+#include    "pantograph_new.h"
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+Pantograph_new::Pantograph_new(QObject *parent) : Device(parent)
+{
+    setY(HEIGHT, height_down);
+
+    name = QString("ks");
+
+    input_signals.resize(SIZE_OF_INPUTS);
+    output_signals.resize(SIZE_OF_OUTPUTS);
+
+    output_signals[OUTPUT_CURRENT] = 0.0;
+    input_signals[INPUT_WIRE_HEIGHT] = 6.0;
+    input_signals[INPUT_VOLTAGE] = 0.0;
+    input_signals[INPUT_FREQUENCY] = 0.0;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+Pantograph_new::~Pantograph_new()
+{
+
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void Pantograph_new::setCurrent(double I)
+{
+    output_signals[OUTPUT_CURRENT] = I;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void Pantograph_new::setPneumodriveFlow(double flow)
+{
+    Qc = flow;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void Pantograph_new::setWireHeight(double h)
+{
+    input_signals[INPUT_WIRE_HEIGHT] = h;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void Pantograph_new::setVoltage(double U)
+{
+    input_signals[INPUT_VOLTAGE] = U;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void Pantograph_new::setFrequency(double f)
+{
+    input_signals[INPUT_FREQUENCY] = f;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+double Pantograph_new::getPneumodrivePressure() const
+{
+    return getY(PRESSURE);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+double Pantograph_new::getHeight() const
+{
+    return getY(HEIGHT);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+double Pantograph_new::getRelativeHeight() const
+{
+    return (getY(HEIGHT) - height_down) / (height_up - height_down);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+double Pantograph_new::getVoltage() const
+{
+    if (is_contact)
+        return input_signals[INPUT_VOLTAGE];
+
+    return 0.0;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+double Pantograph_new::getFrequency() const
+{
+    if (is_contact)
+        return input_signals[INPUT_FREQUENCY];
+
+    return 0.0;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+sound_state_t Pantograph_new::getSoundState(size_t idx) const
+{
+    return ref_state.getSoundState(idx);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+float Pantograph_new::getSoundSignal(size_t idx) const
+{
+    return ref_state.getSoundSignal(idx);
+}
+
+//----------------------------------------------------------------------------
+//
+//----------------------------------------------------------------------------
+void Pantograph_new::ode_system(const state_vector_t &Y, state_vector_t &dYdt, double t)
+{
+    (void) t;
+
+    // Давление в пневмоприводе
+    dYdt[PRESSURE] = Qc / Vc;
+
+    // Высота токоприёмника
+    if (Y[PRESSURE] > Pmin)
+    {
+        // Поднятие токоприёмника
+        // Скорость поднятия с учётом давления в пневмоприводе, м/с
+        double motion_v = motion_v_nom * ((Y[PRESSURE] - Pmin) / (Pnom - Pmin));
+
+        // Целевая высота полоза - высота контактного провода
+        double ref_height = input_signals[INPUT_WIRE_HEIGHT];
+
+        // Проверяем, что есть контактный провод, и его высота
+        // не выше максимально поднятого полоза и не ниже опущенного
+        if ( is_linked &&
+            ( (ref_height < height_up) ||
+             ((ref_height - height_down) > Physics::ZERO) ) )
+        {
+            // Проверяем касание полоза и контактного провода
+            is_contact = (abs(Y[HEIGHT] - ref_height) < eps);
+        }
+        else
+        {
+            // Если провода нет, поднимаем без ограничения
+            ref_height = height_up;
+
+            // Отключаем токоприёмник
+            is_contact = false;
+        }
+
+        // Расстояние до целевой высоты
+        double height_delta = ref_height - Y[HEIGHT];
+
+        // Токоприёмник стремится к проводу
+        dYdt[HEIGHT] = motion_v * (cut(height_delta, -eps, eps) / eps);
+
+        // Озвучка // TODO // Переделать
+        ref_state.set();
+    }
+    else
+    {
+        // Опускание токоприёмника
+        // Скорость опускания токоприёмника с учётом давления в пневмоприводе, м/с
+        double motion_v = motion_v_nom * (Pmin - Y[PRESSURE]);
+
+        // Отключаем токоприёмник
+        is_contact = false;
+
+        // Расстояние до опущенного состояния
+        double height_delta = Y[HEIGHT] - height_down;
+
+        // Токоприёмник стремится опустится
+        dYdt[HEIGHT] = -1.0 * motion_v * (max(height_delta, eps) / eps);
+
+        // Озвучка // TODO // Переделать
+        ref_state.reset();
+    }
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void Pantograph_new::load_config(CfgReader &cfg)
+{
+    QString secName = "Device";
+
+    double tmp = 0.0;
+    cfg.getDouble(secName, "Vc", tmp);
+    if (tmp > 1e-3)
+        Vc = tmp;
+
+    cfg.getDouble(secName, "Pmin", Pmin);
+    cfg.getDouble(secName, "Pnom", Pnom);
+    cfg.getDouble(secName, "MotionTime", motion_time);
+    cfg.getDouble(secName, "HeightDown", height_down);
+    cfg.getDouble(secName, "HeightUp", height_up);
+
+    double motion_v_nom = (height_up - height_down) / motion_time;
+    setY(HEIGHT, height_down);
+}
